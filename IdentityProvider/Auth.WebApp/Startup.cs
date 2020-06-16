b1aeb5fa9c329212923f2413b2137bc7f7c4503d
chuan nguyen <chuannt11673@gmail.com>
2020-06-16 23:28:04 +0700

    add IDP

---
+++ IdentityProvider/Auth.WebApp/Startup.cs
@@ -0,0 +1,65 @@
+using Auth.WebApp.IdentityServer4;
+using IdentityServer4;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Auth.WebApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddIdentityServer()
+               .AddInMemoryIdentityResources(Config.IdentityResources)
+               .AddInMemoryClients(Config.Clients);
+
+            builder.AddDeveloperSigningCredential();
+
+            services.AddAuthentication().AddGoogle("Google", options =>
+            {
+                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                options.ClientId = "1071965663222-9rf9aumd8ga6c80lcmf62ovng2h52957.apps.googleusercontent.com";
+                options.ClientSecret = "5wCD3JnPrg-y5I_pQS2dys3y";
+            });
+
+            services.AddCors(opts =>
+            {
+                opts.AddPolicy("MyPolicy", builder =>
+                {
+                    builder.WithOrigins("http://localhost:8100", "http://localhost:4200")
+                    .AllowCredentials()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
+            services.AddAuthorization();
+            services.AddControllersWithViews().AddXmlSerializerFormatters().AddNewtonsoftJson();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCors("MyPolicy");
+            app.UseIdentityServer();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
---
+++ IdentityProvider/Auth.WebApp/_IdentityServer4/Config.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Auth.WebApp.IdentityServer4
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+        public static IEnumerable<Client> Clients => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "spa angular",
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = true,
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                },
+                RedirectUris = new List<string> { "http://localhost:8100/auth-callback" },
+                PostLogoutRedirectUris = new List<string> { "http://localhost:8100/" },
+                AllowAccessTokensViaBrowser = true
+            },
+            new Client
+            {
+                ClientId = "ionic",
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = true,
+                AllowOfflineAccess = true,
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                },
+                RedirectUris = new List<string> { "https://ionic-hats.com/auth-callback" },
+                PostLogoutRedirectUris = new List<string> { "https://ionic-hats.com/" },
+                FrontChannelLogoutUri = "http://localhost:8100/",
+                AllowAccessTokensViaBrowser = true
+            }
+        };
+    }
+}
